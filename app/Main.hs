263c0c842019db6e5191b49ea0c45757b624f6fd
Pavel Potoček <pavelpotocek@gmail.com>
2021-10-28 10:28:43 +0200

    Use Type.Reflection for type reflection

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE LambdaCase #-}
 
 -- | Example usage of the TypedSerialization library.
 
@@ -9,40 +10,31 @@ import TypedSerialization (TypedSerialize(..), Command, Program, command, progra
 import Text.Read (readMaybe)
 
 
-data TypeRep = Int' | Bool' deriving (Show, Eq)
-
-
-instance TypedSerialize Int String TypeRep where
+instance TypedSerialize Int String where
   serialize = show
   deserialize = readMaybe
-  type_ = Int'
 
-instance TypedSerialize Bool String TypeRep where
+instance TypedSerialize Bool String where
   serialize = show
   deserialize = readMaybe
-  type_ = Bool'
-
-
--- we will serialize both types and values to Strings
-type CommandT = Command String TypeRep
-type ProgramT = Program String TypeRep
 
 
 main :: IO ()
 main = do
   let
+
     -- data will be passed through Strings, but we can still work with Haskell values
     -- without worrying about serialization failures. Ordinary Haskell functions can be lifted
     -- to `Command`s.
 
-    equal :: Int -> CommandT
+    equal :: Int -> Command String
     equal n = command (== n)
 
-    notBool :: CommandT
+    notBool :: Command String
     notBool = command not
 
   -- construct and typecheck a program.
-  notFiveProg <- case program [equal 5, notBool] :: Maybe (ProgramT Int Bool) of
+  notFiveProg <- case program [equal 5, notBool] :: Maybe (Program String Int Bool) of
     Just p -> pure p
     Nothing -> error "Type mismatch"
 
--- src/TypedSerialization.hs
+++ src/TypedSerialization.hs
@@ -7,62 +7,64 @@ module TypedSerialization (Program, TypedSerialize(..), Command, command, progra
 
 import Data.Maybe (fromJust)
 import Data.List (intercalate)
+import Data.Proxy (Proxy(..))
+import Type.Reflection
 
 {- | Serialization with type information
 
 Type parameters:
- * x .. type that we want to serialize (Integer, Bool, etc.)
- * s .. serialization format (ByteString, JSON, etc.)
- * t .. runtime identification of type `x` (Text, JSON, etc.)
+ * x .. type that we want to serialize
+ * s .. serialization format
 
-Class laws:
-1. normal round-trip
-   forall x.  deserialize (serialize x) == Just x
-2. `type_` output corresponds tightly to types
-   forall x1 x2 s t.  @x1 == @x2  <=>  (type_ @x1 @s @t) == (type_ @x2 @s @t)
+This law must hold:
 
-These laws give us the ability to type-check deserializations ahead of time.
+  deserialize . serialize == Just
 -}
-class Eq t => TypedSerialize x s t where
+class Typeable x => TypedSerialize x s where
   serialize   :: x -> s
   deserialize :: s -> Maybe x
-  type_       :: t
 
 
 -- | `Command` is a function (x -> y) transformed to work on serialized data. Type information is stored alongside.
 -- This allows us to defer type checking to run time (performed in the `program` constructor).
-data Command s t = Command
-  t         -- ^ Input type
-  t         -- ^ Output type
-  (s -> s)  -- ^ Function
+data Command s = Command
+  SomeTypeRep  -- ^ Input type
+  SomeTypeRep  -- ^ Output type
+  (s -> s)     -- ^ Function
 
 -- | Construct a `Command`, saving type information as runtime values
 --
 -- `fromJust` is safe here, since we can evaluate only a program type-checked by `program`
-command :: forall s t x y. (TypedSerialize x s t, TypedSerialize y s t) => (x -> y) -> Command s t
-command f = Command (type_ @x @s @t) (type_ @y @s @t) (serialize @y @s @t . f . fromJust . deserialize @x @s @t)
+command :: forall s x y. (TypedSerialize x s, TypedSerialize y s) => (x -> y) -> Command s
+command f = Command
+  (someTypeRep (Proxy @x))
+  (someTypeRep (Proxy @y))
+  (serialize @y @s . f . fromJust . deserialize @x @s)
 
 
 -- | Chain of commands which can be applied in a sequence. That is, the output type of a command
 -- matches the input type of the next command. Also, the input type of the first command is `x`,
 -- and the output type of the last program is `y`.
-data Program s t x y = Program [Command s t]
+data Program s x y = Program [Command s]
 
 -- | Show the sequence of types inside a `Program` as t1 ~> t2 ~> ... ~> tN.
-showTypes :: forall s t x y. (TypedSerialize x s t, Show t) => Program s t x y -> String
-showTypes (Program cs) = intercalate " ~> " $ map show (type_ @x @s @t : map ty cs) where
+showTypes :: forall s x y. (TypedSerialize x s) => Program s x y -> String
+showTypes (Program cs) = intercalate " ~> " $ map show (someTypeRep (Proxy @x) : map ty cs) where
   ty (Command _ t _) = t
 
 -- | Construct a program, validating correct type usage.
-program :: forall s t x y. (TypedSerialize x s t, TypedSerialize y s t) => [Command s t] -> Maybe (Program s t x y)
-program cs = if verify (type_ @x @s @t) (type_ @y @s @t) cs then Just (Program cs) else Nothing where
+program :: forall s x y. (TypedSerialize x s, TypedSerialize y s) => [Command s] -> Maybe (Program s x y)
+program cs = if verify (someTypeRep (Proxy @x)) (someTypeRep (Proxy @y)) cs
+  then Just (Program cs)
+  else Nothing
+  where
 
-  verify :: t -> t -> [Command s t] -> Bool
+  verify :: SomeTypeRep -> SomeTypeRep -> [Command s] -> Bool
   verify x y [] = x == y
   verify x y (Command a b f : cs) = x == a && verify b y cs
 
 -- | Evaluate a program. This cannot fail, because only valid programs can be constructed by `program`.
-eval :: forall x y s t. (TypedSerialize x s t, TypedSerialize y s t) => Program s t x y -> x -> y
-eval (Program cs) = fromJust . deserialize @y @s @t . go cs . serialize @x @s @t where
+eval :: forall x y s. (TypedSerialize x s, TypedSerialize y s) => Program s x y -> x -> y
+eval (Program cs) = fromJust . deserialize @y @s . go cs . serialize @x @s where
   go [] x = x
   go (Command _ _ f : cs) x = go cs (f x)
